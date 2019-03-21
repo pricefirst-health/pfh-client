c7031fd8735c65398703b95f584fc5a1311753e4
Sujan Baral <sujan@turo.com>
2019-03-21 01:49:11 -0400

    Initial commit

---
+++ src/containers/app/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Route, Link, NavLink } from 'react-router-dom'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Home from '../home'
+import About from '../about'
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => (
+  <div>
+    <header>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand><Link to="/">PF Health</Link></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavLink to="/" className={"nav-link"}>Home</NavLink>
+                    <NavLink to="/about-us" className={"nav-link"}>About us</NavLink>
+                </Nav>
+                <NavDropdown title="Sujan" id="basic-nav-dropdown" alignRight={true}>
+                    <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Dashboard</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.3">Logout</NavDropdown.Item>
+                </NavDropdown>
+            </Navbar.Collapse>
+        </Navbar>;
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+)
+
+export default App
